8443b5f4c8e12c66328ece780ca6eb4f1b2ecf0a
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-15 20:53:29 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,9 +38,7 @@ class TweetsController < ApplicationController
     params.permit(:image, :text)
   end
 
-  private
   def move_to_index
-
     redirect_to action: :index unless user_signed_in?
   end
 
